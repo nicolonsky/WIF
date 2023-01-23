aed8c19fda6bf4f60beeba98bf246b7c33b8c4e5
Nicola Suter <nicola@nicolonsky.ch>
2023-01-23 11:27:19 +0100

    Init

---
+++ README.md
@@ -0,0 +1,53 @@
+# GitHub Action for Azure AD Workload Identity JWTs
+
+This GitHub action acquires an access token (JWT) for an Azure AD Workload Identity that has configured GitHub as OIDC credential provider.
+The access token can be used for any kind of API access or usage, like Microsoft Graph. The retrieved access token is stored as environment variable `ACCESS_TOKEN` within the GitHub Action workflow and can also be consumed from PowerShell.
+
+![WIF Overview](https://learn.microsoft.com/en-us/azure/active-directory/develop/media/workload-identity-federation/workflow.svg)
+
+## Usage
+
+Tha action requires two mandatory inputs:
+
+* Azure AD Tenant ID
+* Azure AD App Registration Client ID
+
+You can also pass the inputs via GitHub repository secrets to the GitHub action.
+
+```yaml
+  - name: Acquire Workload Identity Access Token
+        uses: ./
+        id: wif
+        with:
+          tenant_id: '30204efd-acb7-41a7-9fe9-233cec0556c1'
+          client_id: '960d282a-7d02-453e-a680-3b4330ed38a7'
+```
+
+Make sure to add the following permissions to your action:
+
+```yaml
+permissions:
+  id-token: write
+  contents: read
+```
+
+The  retrieved access token can then used like this:
+
+```powershell
+Install-Module -Name Microsoft.Graph.Authentication
+Connect-MgGraph -AccessToken $env:ACCESS_TOKEN
+Invoke-MgGraphRequest -Uri '/beta/organization' | Select-Object -ExpandProperty value
+```
+
+## Development
+
+Building the action:
+
+```bash
+ncc build index.js -o dist
+```
+
+## Ressources
+
+* <https://learn.microsoft.com/en-us/azure/active-directory/develop/workload-identity-federation>
+* <https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow#third-case-access-token-request-with-a-federated-credential>
---
+++ index.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+function parseJwt(token) {
+    return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+}
+
+try {
+    const tenantId = core.getInput('tenant_id', { required: true });
+    const clientId = core.getInput('client_id', { required: true });
+    const audience = core.getInput('audience', { required: false });
+    const tokenEndpoint = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`
+
+    core.getIDToken(audience).then(token => {
+        core.debug(`Acquired GitHub OIDC Token`)
+        core.debug(parseJwt(token))
+        // POST Request to the Azure AD Token Endpoint to exchange GitHub JWT
+        // https://learn.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow
+        axios.post(tokenEndpoint, {
+            scope: 'https://graph.microsoft.com/.default',
+            client_id: clientId,
+            client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+            grant_type: 'client_credentials',
+            client_assertion: token
+        }, {
+            headers: { 'content-type': 'application/x-www-form-urlencoded' }
+        }).then(response => {
+            core.info("Successfully acquired Azure AD access token!")
+            core.setOutput("access_token", response.data['access_token']);
+            core.exportVariable('ACCESS_TOKEN', response.data['access_token']);
+            core.debug(parseJwt(response.data['access_token']))
+        }).catch(({ response }) => {
+            core.error(response.data)
+            core.setFailed(`Failed to Acquire Azure AD JWT: ${error.message}`)
+        })
+    }).catch(error => {
+        core.setFailed(`Failed to Acquire GitHub Action Token: ${error.message}`)
+        core.error(error.message)
+    })
+} catch (error) {
+    core.setFailed(error.message);
+}
